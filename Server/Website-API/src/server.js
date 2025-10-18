d60f2c31110f4318b0e48ed9f37998302cf57155
Kaiwaii4ever <Kaiwaii4ever@gmail.com>
2025-10-19 00:24:04 +0900

    1

--- Server/Website-API/src/server.js
+++ Server/Website-API/src/server.js
@@ -18,12 +18,30 @@ if (process.env.NODE_ENV !== 'production') {
   dotenv.config({ path: path.resolve(__dirname, '../../../.env') });
 }
 
+const allowedOrigins = [
+  "https://kaiwaii4ever.win",
+  "http://localhost:5173"
+];
+
 const app = express();
 
 app.use(express.json());
 app.use(logger);
-app.use(cors({ origin: 'https://kaiwaii4ever.win' }));
-app.use(cors({ origin: 'http://localhost:5173' }));
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.get('/', (req, res) => res.send('Backend is running'));
 
